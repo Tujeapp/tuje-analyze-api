fc02df7cf79a16fe09352b7c4ad9b83c6b0aeeca
Tujeapp <app.tuje@gmail.com>
2025-07-31 20:38:05 +0700

    main.py

--- main.py
+++ main.py
@@ -529,4 +529,5 @@ async def webhook_sync_vocab(entry: VocabEntry):
 # Local testing
 # ----------------------
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
