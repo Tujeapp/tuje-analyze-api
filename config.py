fa133179a34ed509b3ad71fd919d62f970900938
Tujeapp <app.tuje@gmail.com>
2025-08-01 10:57:26 +0700

    config.py

--- config.py
+++ config.py
@@ -3,22 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load from .env if available
 
-----------
+
 # PostgreSQL
-----------
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("Missing environment variable: DATABASE_URL")
 
-----------
 # OpenAI
-----------
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
     raise RuntimeError("Missing environment variable: OPENAI_API_KEY")
 
-----------
 # Airtable
-----------
 AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
