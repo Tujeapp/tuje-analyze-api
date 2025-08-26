dfeec2adee491493f4c578dfa551ae2b929c0670
Tujeapp <app.tuje@gmail.com>
2025-08-26 11:27:54 +0700

    adjustement_french_contractions.py

---
+++ adjustement_french_contractions.py
@@ -0,0 +1,16 @@
+# adjustement_french_contractions.py
+"""French contraction patterns"""
+
+CONTRACTION_PATTERNS = {
+    r"\bj'ai\b": "jai",
+    r"\bj'": "j ",
+    r"\bd'": "de ",
+    r"\bl'": "le ",
+    r"\bc'est\b": "cest",
+    r"\bc'": "ce ",
+    r"\bqu'": "que ",
+    r"\bn'": "ne ",
+    r"\bt'": "te ",
+    r"\bs'": "se ",
+    r"\bm'": "me "
+}
