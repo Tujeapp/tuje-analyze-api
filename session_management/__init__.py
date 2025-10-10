4730b9f12bfd402200aa32db2c92937c795ff818
Tujeapp <app.tuje@gmail.com>
2025-10-10 17:22:04 +0700

    __init__.py

--- session_management/__init__.py
+++ session_management/__init__.py
@@ -6,11 +6,13 @@ from .session_service import session_service
 from .cycle_service import cycle_service
 from .interaction_service import interaction_service
 from .answer_service import answer_service
+from .scoring_service import scoring_service
 
 # Export all services
 __all__ = [
     'session_service',
     'cycle_service',
     'interaction_service',
-    'answer_service'
+    'answer_service',
+    'scoring_service'
 ]
